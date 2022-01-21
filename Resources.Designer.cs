64e0944b88feeaeea7e61431a1e7a0180850f4c9
Earthmark (Daniel Miller) <earthmark.miller@gmail.com>
2022-01-20 21:23:06 -0800

    Added an icon which uses Icon, seeing if that binds.

--- Resources.Designer.cs
+++ Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace GithubActionsResourcesTest {
                 return ((byte[])(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon VSPackage {
+            get {
+                object obj = ResourceManager.GetObject("VSPackage", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
